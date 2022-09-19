fd1cf777ebdbafa39dc811c5890733f9fc7f6cd2
Jeong-Yoon-Hwan <wjddbsghks5742@gmail.com>
2022-09-20 06:05:11 +0900

    주석

--- Header.js
+++ Header.js
@@ -4,11 +4,11 @@ const Header = (element,text) =>{
     x=> temp.push(x)
   )
   let styling= temp.join(';');
-  
+  // headerSyle 객체를 forEach로 반복시키고 temp에 저장함, styling이라는 객체를 만들어서 join()사용하여 세미콜론으로 이어붙임
+
   return (
     element.innerHTML=`<div style=${styling}>${text}</div>`
   ) 
-  
 }
 
 
